9340f4041821ada176e9c59857e9cdd9574d805a
Tyler Messenger-lehmann <100587501@oncampus.local>
2017-02-04 23:22:43 -0500

    TwitterAPI.py changed to work on Python 3.5

--- TwitterAPI.py
+++ TwitterAPI.py
@@ -19,11 +19,10 @@ auth.set_access_token(access_token, access_token_secret)
 # Creation of the actual interface, using authentication
 api = tweepy.API(auth)
 # User name
-name = str(sys.argv)[18:-2]
+name = str(sys.argv)[19:-2]
 # Sample method, used to update a status
 # api.update_status('All these tweets are generated using API!')
 
 # Get the User object for twitter...
 user = api.get_user(name)
-print user.screen_name
-print user.followers_count
+print(user.followers_count)
